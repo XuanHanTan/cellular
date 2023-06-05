69db44dd2f064d6a1ec68948c708bd2f88a10526
Xuan Han Tan <43411204+XuanHanTan@users.noreply.github.com>
2023-06-05 22:10:39 +0800

    Initial Commit

---
+++ Cellular/CellularApp.swift
@@ -0,0 +1,17 @@
+//
+//  CellularApp.swift
+//  Cellular
+//
+//  Created by Xuan Han on 5/6/23.
+//
+
+import SwiftUI
+
+@main
+struct CellularApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
